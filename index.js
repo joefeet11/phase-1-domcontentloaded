537e9ccfaf55456cc95712b2dc9de0b0cabf0176
Kevin Olivier <kevinolivier1111@gmail.com>
2022-09-22 16:51:40 -0600

    commit tree 692a894415df1c15efb4226b179d0cb1bc0f6f44

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let p = document.getElementById('text')
+    console.log(p)
+    p.textContent = "This is really cool!"
+    console.log(p)
+    
+
+  });
